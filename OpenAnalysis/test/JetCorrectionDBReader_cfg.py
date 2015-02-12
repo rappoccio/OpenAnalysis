ee13834f1ca22671fd5f93e5b300092b46136e1d
Salvatore Rappoccio <rappoccio@gmail.com>
2015-02-12 11:36:24 -0500

    Switching to correct global tag

--- OpenAnalysis/test/JetCorrectionDBReader_cfg.py
+++ OpenAnalysis/test/JetCorrectionDBReader_cfg.py
@@ -4,7 +4,7 @@ process = cms.Process("myprocess")
 
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = 'GR_R_42_V24::All'
+process.GlobalTag.globaltag = 'GR_R_41_V0::All'
 
 process.maxEvents = cms.untracked.PSet(
         input = cms.untracked.int32(1)
@@ -14,13 +14,13 @@ process.source = cms.Source("EmptySource")
 
 process.ak5 = cms.EDAnalyzer('JetCorrectorDBReader', 
         payloadName    = cms.untracked.string('AK5PF'),
-	globalTag      = cms.untracked.string('GR_R_42_V24'),  
+	globalTag      = cms.untracked.string('GR_R_41_V0'),  
         printScreen    = cms.untracked.bool(False),
         createTextFile = cms.untracked.bool(True)
 )
 process.ak7 = cms.EDAnalyzer('JetCorrectorDBReader', 
         payloadName    = cms.untracked.string('AK7PF'),
-	globalTag      = cms.untracked.string('GR_R_42_V24'),  
+	globalTag      = cms.untracked.string('GR_R_41_V0'),  
         printScreen    = cms.untracked.bool(False),
         createTextFile = cms.untracked.bool(True)
 )
